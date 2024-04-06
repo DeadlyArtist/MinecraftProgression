5aaa303c0ec3459f77a1beecb1e9b3265ac333b1
Deadly Artist <ocram.siebenstein@web.de>
2024-04-06 23:07:08 +0200

    INIT

---
+++ src/MinecraftProgression/src/main/java/com/deadlyartist/minecraftprogression/Progression.java
@@ -0,0 +1,37 @@
+package com.deadlyartist.minecraftprogression;
+
+import com.deadlyartist.minecraftprogression.init.BlockInit;
+import com.deadlyartist.minecraftprogression.init.ItemInit;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLanguageProvider;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("minecraftprogression")
+public class Progression {
+	public static final String MODID = "minecraftprogression";
+	public static final String MAIN_TAB_ID = MODID + "_0";
+	public static final CreativeModeTab MAIN_TAB = new CreativeModeTab(MAIN_TAB_ID) {
+		
+		@Override
+		@OnlyIn(Dist.CLIENT)
+		public ItemStack makeIcon() {
+			return new ItemStack(ItemInit.INGOT_STEEL.get());
+		}
+	};
+	
+	public Progression() {
+		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+		
+		ItemInit.ITEMS.register(bus);
+		BlockInit.BLOCKS.register(bus);
+		
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+}
