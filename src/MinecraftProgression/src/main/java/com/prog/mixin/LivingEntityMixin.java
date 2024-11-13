c3c237a57f8edc543e44bbab2472c230674c6546
Deadly Artist <ocram.siebenstein@web.de>
2024-11-13 04:32:29 +0100

    remove more dependencies

--- src/MinecraftProgression/src/main/java/com/prog/mixin/LivingEntityMixin.java
+++ src/MinecraftProgression/src/main/java/com/prog/mixin/LivingEntityMixin.java
@@ -10,10 +10,12 @@ import com.prog.utils.UseUtils;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.EnchantedBookItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -22,9 +24,11 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -32,7 +36,11 @@ import java.util.HashSet;
 import java.util.List;
 
 @Mixin(LivingEntity.class)
-public class LivingEntityMixin {
+public abstract class LivingEntityMixin {
+
+    @Shadow public abstract void damageArmor(DamageSource source, float amount);
+
+    @Shadow public abstract boolean damage(DamageSource source, float amount);
 
     @Unique
     private int ticksSince = 0;
@@ -124,5 +132,36 @@ public class LivingEntityMixin {
             var stack = EnchantedBookItem.forEnchantment(EnchantmentUtils.getRandomEnchantmentLevelEntry(self.random, enchantmentLevel, false, excludedEnchantments));
             self.dropStack(stack);
         }
+    }
+
+    @Inject(method = "applyArmorToDamage", at = @At("HEAD"), cancellable = true)
+    private void injectApplyArmorToDamage(DamageSource damageSource, float damage, CallbackInfoReturnable<Float> cir) {
+        if (damageSource.bypassesArmor()) return;
+        damageArmor(damageSource, damage);
+
+        var self = (LivingEntity) (Object) this;
+        var armor = self.getArmor();
+        var toughness = (float) self.getAttributeValue(EntityAttributes.GENERIC_ARMOR_TOUGHNESS);
+        var hitpct = Math.max(0, Math.min(1, damage / self.getMaxHealth()));
+
+        // https://deadlyartist.github.io/aidevsuite/#extern?url=data/Live%20Calculator.json&mode=run
+        // damage = 1
+        // armor = 10
+        // offset = 25
+        // formula1 = damage * offset / (offset + armor)
+        // formula2 = damage * 1 / (1 + armor / 10);
+        // [formula1, formula2].join("    ")
+        var offset = 25;
+        damage = damage * ((float) offset / (armor + offset));
+
+        // https://deadlyartist.github.io/aidevsuite/#extern?url=data/Live%20Calculator.json&mode=run
+        // damage = 1
+        // toughness = 10
+        // hitpct = 0.25 // (0-1) relative health lost
+        // formula1 = damage * (1 / (toughness / 10 + 1) * hitpct + (1 - hitpct));
+        // [formula1, formula2].join("    ")
+        damage = damage * (1 / (toughness / 10 + 1) * hitpct + (1 - hitpct));
+
+        cir.setReturnValue(Math.max(0.5f, damage));
     }
 }
