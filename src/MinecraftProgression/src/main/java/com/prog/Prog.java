f7ae821c6ebe68b9281df2d0583e921a94ada68c
Deadly Artist <ocram.siebenstein@web.de>
2025-03-24 16:55:15 +0100

    bump version

--- src/MinecraftProgression/src/main/java/com/prog/Prog.java
+++ src/MinecraftProgression/src/main/java/com/prog/Prog.java
@@ -51,7 +51,7 @@ import static com.prog.entity.attribute.PEntityAttributes.IMMUNITY_MAP;
 
 public class Prog implements ModInitializer {
     public static final String MOD_ID = "prog";
-    public static final String VERSION = "1.2.2";
+    public static final String VERSION = "1.2.3";
     public static final String NAME = "More Progression";
     public static final Logger __LOGGER = LoggerFactory.getLogger(MOD_ID);
 
